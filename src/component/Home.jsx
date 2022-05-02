c7828354808f447057acd9ee8c3eb0014b50983a
sketodee <shobandejames@gmail.com>
2022-05-02 20:07:26 +0100

    second commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                     <img src="/assets/bgim.jpg" class="card-img" alt="background image" height="550px" />
                     <div className="card-img-overlay d-flex flex-column justify-content-around">
                         <div className="container text-light">
-                            <h5 className="card-title display-3 fw-bold mb-o"> CHECK OUT NEW ARRIVALS</h5>
+                            <h5 className="card-title display-3 fw-bold mb-o"> CHECK OUT SOME ARRIVALS</h5>
                             <p className="card-text">EXPLORE LATEST TRENDS</p>
                         </div>
                     </div>
